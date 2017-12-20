c98839a430321735d1dd4adb56140ab7c16cc265
EliasJany <31951013+EliasJany@users.noreply.github.com>
2017-12-20 18:09:06 +0100

    Add files via upload

---
+++ assignment-3.pl
@@ -0,0 +1,13 @@
+head(H, [H|_]).
+tail(T, [_|T]).
+
+prepend(E, T, [E|T]).
+
+addElement(E, [], [E]).
+addElement(E,[H|T], [H|AT]) :- addElement(E,T,AT).
+
+hasLength([],0).
+hasLength([_|T],L) :- hasLength(T,N), L is N+1.
+
+remove(E, [E|T], T).
+remove(E,[H|T], [H|RT]) :- remove(E,T,RT).
